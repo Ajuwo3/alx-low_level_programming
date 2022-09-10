b5a861ea870277f75713553ce1df256f1aed05db
Ajuwo3 <https://github.com/Ajuwo3/alx-zero_day.git>
2022-09-10 14:08:26 -0700

    Programming is like building a multi lingual puzzle

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,12 @@
 #include <stdio.h>
 
 /**
- * main - prints exactly "Programming is like building a multilingual puzzle.
+ * main - return "Programming is like building a multilingual puzzle"
  *
  * Return: Always 0 (Success)
  */
-
 int main(void)
 {
-	puts("\"Program is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
