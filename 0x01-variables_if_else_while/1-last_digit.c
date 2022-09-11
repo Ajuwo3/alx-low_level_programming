42d885eeff61b293d7a9943fc28cd649c151b777
Ajuwo3 <https://github.com/Ajuwo3/alx-zero_day.git>
2022-09-11 12:09:54 -0700

    The last digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -2,7 +2,8 @@
 #include <time.h>
 #include <stdio.h>
 /**
- * main - assign a random number to the variable
+ * main - assign a random number to the variable.
+ * Description: Entry point.
  *
  * Return: Always 0 (Success)
  */
@@ -16,7 +17,7 @@ int main(void)
 	m = n % 10;
 	if (m > 5)
 	{
-		printf("Last digit of %d is %d and is greater than 5\n", n, m)
+		printf("Last digit of %d is %d and is greater than 5\n", n, m);
 	}
 	else if (m < 6 && m != 0)
 	{
@@ -27,3 +28,4 @@ int main(void)
 		printf("Last digit of %d is %d and is 0\n", n, m);
 	}
 	return (0);
+}
