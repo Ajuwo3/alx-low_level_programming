6c2686265c3dfa712c400cf326125e329987b5c3
Ajuwo3 <https://github.com/Ajuwo3/alx-zero_day.git>
2022-09-14 07:52:27 -0700

    a function that prints the alphabet, in lowercase, followed by a new line

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_alphabet - function to print a b c.
+ * Return: Always 0
+ *
+ */
+void print_alphabet(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		_putchar(c);
+	}
+_putchar('\n');
+}
