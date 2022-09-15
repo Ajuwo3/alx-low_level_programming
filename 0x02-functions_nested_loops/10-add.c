975d219fee1ac424b021cbfd9f32596a70b576eb
Ajuwo3 <https://github.com/Ajuwo3/alx-zero_day.git>
2022-09-15 07:32:09 -0700

    function that adds two integers and returns the result

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * add - function that adds two integers and returns the result
+ * Return: Always 0
+ * @num1: first integer
+ * @num2: second integer
+ */
+int add(int num1, int num2)
+{
+	return (num1 + num2);
+}
