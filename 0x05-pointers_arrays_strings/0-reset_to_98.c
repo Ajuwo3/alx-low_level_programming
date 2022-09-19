1188630445660abb01f9c20473841aa1c631b670
Ajuwo3 <https://github.com/Ajuwo3/alx-zero_day.git>
2022-09-19 07:33:16 -0700

    Reset value of given integer to another

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * reset_to_98 - reset value of given integer to 98
+ * @n: parameter
+ * Return: Always 0
+ */
+
+void reset_to_98(int *n)
+{
+	int n;
+	int *n;
+
+	n = 402;
+	n = &n;
+	*n = 98
+}
+
+
