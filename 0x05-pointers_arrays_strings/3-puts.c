f5f1bf46aa5702ccc9e1823c5199dca623c4f3d1
Ajuwo3 <https://github.com/Ajuwo3/alx-zero_day.git>
2022-09-19 20:46:05 -0700

    function that prints a string

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _puts - function that prints a string
+ * @str: string to be printed
+ * Return: Always 0
+ */
+void _puts(char *str)
+{
+	int i = 0;
+
+	while (*(str + i) != '\0')
+	{
+		putchar(*(str + i));
+		i++;
+	}
+	putchar(0);
+}
