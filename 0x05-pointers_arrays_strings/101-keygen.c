5310b0690be99234d74336c9e079b5696a4c7ee3
Ajuwo3 <https://github.com/Ajuwo3/alx-zero_day.git>
2022-09-21 01:16:53 -0700

    program that generates random valid passwords

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - entry point
+ * Description: program that generates random valid passwords
+ * for a program
+ * Return: generated password
+ */
+int main(void)
+{
+	char c;
+	int x;
+
+	srand(time(0));
+	while (x <= 2645)
+	{
+		c = rand() % 128;
+		x += c;
+		putchar(c);
+	}
+	putchar(2772 - x);
+
+	return (0);
+}
