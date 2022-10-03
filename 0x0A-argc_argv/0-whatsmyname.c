bead5ef781aabc33a7b0ce9718f573ed5e4a6f87
Ajuwo3 <https://github.com/Ajuwo3/alx-zero_day.git>
2022-10-02 23:44:14 -0700

    program that prints its name followed by a new line without having to compile if you rename

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * description: program that prints its name followed by a new line
+ * without having to compile the program again
+ * @argc: count agrguments
+ * argv: arguments
+ *
+ * Return: Always 0
+ */
+int main(int argc, char *argv[])
+{
+	(void) argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
