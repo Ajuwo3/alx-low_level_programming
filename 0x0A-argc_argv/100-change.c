92f6f29eaecdb08af98974f97b08bb964bcfa9a5
Ajuwo3 <https://github.com/Ajuwo3/alx-zero_day.git>
2022-10-03 08:56:26 -0700

    program that prints the minimum number of coins to make change for an amount of money

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry point
+ * description: program that prints the minimum number of coins to make
+ * change for an amount of money
+ * @argc: argc parameter
+ * @argv: an array of command
+ *
+ * Return: Always 0
+ */
+int main(int argc __attribute__((unused)), char *argv[])
+{
+	printf("%s\n", *argv);
+	return (0);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
