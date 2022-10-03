effe8813998522870e33ca4c4e857b174dfff267
Ajuwo3 <https://github.com/Ajuwo3/alx-zero_day.git>
2022-10-03 08:27:53 -0700

    program that adds positive numbers

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,39 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ * description: program that adds positive numbers
+ * @argc: argc parameter
+ * @argv: an array of a command
+ *
+ * Return: Always 0
+ */
+int main(int argc, char *argv[])
+{
+	int result = 0, num, i, j, k;
+
+	if (argc == 1)
+		printf("0\n");
+
+	for (i = 1; i < argc; i++)
+	{
+		for (j = 0; argv[i][j] != '\0'; j++)
+		{
+			if (argv[i][j] > '9' || argv[i][j] < '0')
+			{
+				printf("%s\n", "Errpr");
+				return (1);
+			}
+		}
+	}
+
+	for (k = 1; k < argc; k++)
+	{
+		num = atoi(argv[k]);
+		result += num;
+	}
+	printf("%d\n", result);
+	return (0);
+}
