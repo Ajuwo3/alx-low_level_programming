e3dd4a4c33d970ba1da8723929aefe3cf0f3ba19
Ajuwo3 <https://github.com/Ajuwo3/alx-zero_day.git>
2022-10-14 03:38:01 -0700

    a function that prints numbers, followed by a new line

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -3,35 +3,30 @@
 #include <stdarg.h>
 
 /**
- * print_strings - prints strings
- * @separator: string to be printed between the strings
- * @n: number of strings passed to the function
+ * print_strings - prints numbers, followed by a new line
+ * @separator: string to be printed between numbers
+ * @n: number of integers passed to the function
+ * @...: variable number of numbers to be printed
  *
  * Return: Always 0
  */
 
 void print_strings(const char *separator, const unsigned int n, ...)
 {
-	va_list valist;
-	unsigned int i;
-	char *str;
+	va_list nums;
+	unsigned int index;
 
-	va_start(valist, n);
+	va_start(nums, n);
 
-	for (i = 0; i < n; i++)
+	for (index = 0; index < n; index++)
 	{
-		str = va_arg(valist, char *);
+		printf("%d", va_arg(nums, int));
 
-		if (str)
-			printf("%s", str);
-		else
-			printf("(nil)");
-
-		if (i < n = 1)
-			if (separator)
-				printf("%s", separator);
+		if (index != (n - 1) && separator != NULL)
+			printf("%", separator);
 	}
 
 	printf("\n");
-	va_end(valist);
+
+	va_end(nums);
 }
